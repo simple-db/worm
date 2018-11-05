0cc320dd9b42be140da8ae49ece19793447ca820
wtzhuque <w.t.zhuque@gmail.com>
2018-11-06 00:33:08 +0800

    add table

---
+++ src/table.go
@@ -0,0 +1,10 @@
+package table
+
+import (
+    "fmt"
+)
+
+func open_table(table_path string) bool {
+    fmt.Println("open table", table_path)
+    return true
+}
--- src/worm.go
+++ src/worm.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+    "flag"
     "fmt"
 )
 
-const service_name string = "Worm"
+func start_service(port int) bool {
+    if port == 8080 {
+        return true
+    } else {
+        return false
+    }
+}
 
 func main() {
-    var port int = 8080
-    fmt.Println("Worm start on port", port)
+    var port = flag.Int("port",  8338, "port to listen on")
+    var cache = flag.String("cache", "./data", "cache data path")
+    var deamon = flag.Bool("deamon", false, "work in deamon mode")
+
+    flag.Parse()
+
+    var succ = start_service(*port)
+
+    if succ {
+        fmt.Println("Worm start on port", *port)
+    }
+
+    succ = open_table(*cache)
+
+    if succ {
+        fmt.Println("Worm open table", *cache)
+    }
+
+    if *deamon {
+        fmt.Println("deamon")
+    }
 }
